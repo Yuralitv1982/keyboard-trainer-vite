57abe375f6650d57637106a4353c3824d8e471b7
Yuralitv1982 <yuralitv02@gmail.com>
2025-04-13 16:16:05 +0300

    add input and style correct and incorrect

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,32 @@
 import { getJoke } from './api.js';
 
 getJoke();
+
+const userInput = document.getElementById('user-input');
+const targetTextElement = document.getElementById('target-text');
+
+let typedText = '';
+let targetText = '';
+
+userInput.addEventListener('input', () => {
+   typedText = userInput.value;
+   targetText = targetTextElement.textContent;
+
+   highlightText();
+});
+
+function highlightText() {
+   let highlightedText = '';
+   for (let i = 0; i < targetText.length; i++) {
+      if (i < typedText.length) {
+         if (typedText[i] === targetText[i]) {
+            highlightedText += `<span class="correct">${targetText[i]}</span>`;
+         } else {
+            highlightedText += `<span class="incorrect">${targetText[i]}</span>`;
+         }
+      } else {
+         highlightedText += targetText[i];
+      }
+   }
+   targetTextElement.innerHTML = highlightedText;
+}
