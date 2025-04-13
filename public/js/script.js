06199458280960adbcba9623eac2fa6a92da8700
Yuralitv1982 <yuralitv02@gmail.com>
2025-04-13 14:29:47 +0300

    add js files game and script

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,31 @@
 console.log('hello');
+// script.js
+// import { generateText } from './api.js';
+// import { loadLesson } from './lesson.js';
+
+const userInputElement = document.getElementById('user-input');
+userInputElement.addEventListener('input', handleInput);
+
+async function getLessonFiles() {
+   // Получение списка файлов уроков
+}
+
+async function getLessonText(lessonFileName) {
+   // Загрузка текста урока
+}
+
+async function getApiText() {
+   // Получение шутки от API
+}
+
+async function getRandomText() {
+   // Генерация случайных слов
+}
+
+async function loadText() {
+   try {
+      // Загрузка текста
+   } catch (error) {
+      // Отображение ошибки
+   }
+}
