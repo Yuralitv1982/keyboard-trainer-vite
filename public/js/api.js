c2f69bd0067c26f4ec1563feb5886a6e50eee81c
Yuralitv1982 <yuralitv02@gmail.com>
2025-04-13 15:55:01 +0300

    add export from api.js

---
+++ public/js/api.js
@@ -0,0 +1,21 @@
+export async function getJoke() {
+   try {
+      const response = await fetch(API_URL, {
+         headers: {
+            Accept: 'application/json',
+         },
+      });
+      if (!response.ok) {
+         throw new Error(`HTTP ошибка! статус: ${response.status}`);
+      }
+      const data = await response.json();
+      if (data && data.joke) {
+         jokeTextElement.textContent = data.joke;
+      } else {
+         jokeTextElement.textContent = 'Не удалось получить шутку.';
+      }
+   } catch (error) {
+      console.error('Ошибка при получении шутки:', error);
+      jokeTextElement.textContent = 'Не удалось получить шутку.';
+   }
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,6 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 const jokeTextElement = document.getElementById('target-text');
 
-async function getJoke() {
-   try {
-      const response = await fetch(API_URL, {
-         headers: {
-            Accept: 'application/json',
-         },
-      });
-      if (!response.ok) {
-         throw new Error(`HTTP ошибка! статус: ${response.status}`);
-      }
-      const data = await response.json();
-      if (data && data.joke) {
-         jokeTextElement.textContent = data.joke;
-      } else {
-         jokeTextElement.textContent = 'Не удалось получить шутку.';
-      }
-   } catch (error) {
-      console.error('Ошибка при получении шутки:', error);
-      jokeTextElement.textContent = 'Не удалось получить шутку.';
-   }
-}
+import { getJoke } from './api';
 
 getJoke();
